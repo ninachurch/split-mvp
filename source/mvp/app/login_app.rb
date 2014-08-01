c19a605c73fd2a64a09efaaf205b1f3c37ca1594
N!N@ 6HUR6H <ninapearlchurch@gmail.com>
2014-08-01 16:47:19 -0700

    Added sinatra interface

---
+++ source/mvp/app/login_app.rb
@@ -0,0 +1,35 @@
+require 'rubygems'
+require 'sinatra'
+require '../db/config.rb'
+require_relative './models/user'
+
+# get '/hello' do
+#     user = User.first
+#     name = user.first_name
+#     @string_name = name.to_s
+#   # "Hello " + string_name + "!"
+#   erb :welcome
+# end
+
+get '/' do
+  # @user = User.create(params)
+  erb :index
+end
+
+post "/new" do
+  @user = User.create(params)
+  redirect '/welcome'
+end
+
+get '/welcome' do
+  erb :welcome
+end
+
+# get '/' do
+#   @user = User.create("created_at DESC")
+#   redirect "/new" if @user.empty?
+#   erb :index
+#     # @signed_up = is_following?(@user1, @user2)  erb :follows
+# end
+
+
